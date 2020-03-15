b35c03c9ead05a24b092074ff6187a464e4d8de2
Thanh <hades.n20@gmail.com>
2020-03-15 23:09:03 +0700

    socket ver1

---
+++ client.py
@@ -0,0 +1,11 @@
+import socket
+
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.connect((socket.gethostname(), 1234))
+
+
+msg = s.recv(1024)
+print(msg.decode("utf-8"))
+serversocket
+serversocket.send(bytes("Welcome to the server!", "utf-8"))
+s.close()
